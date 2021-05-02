ac684bf62cdd4910cd4893089e1d48af51d18d85
The_Administrator <mr.luke.swanson@gmail.com>
2021-05-02 08:46:17 -0500

    Tinkering with config files and restructuring

--- gary_maker/__init__.py
+++ gary_maker/__init__.py
@@ -1,3 +1,14 @@
-import pandas as pd
-import numpy as np
-import gary_df_maker, chunkstring, stud_maker
+import configparser
+import sys,os
+from df_maker import df_maker
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+data = config.items('GARY_IN')
+per_file, fam_file, per_var, fam_var, per_pos, fam_pos = [x[1] for x in data]
+
+fam_var = fam_var.split(",")
+per_pos, fam_pos = [x for x in tuple(map(eval, per_pos.split('\n')))], [x for x in tuple(map(eval, fam_pos.split("\n")))]
+df = df_maker(per_file, fam_file, per_var, fam_var, per_pos, fam_pos)
+print(df)
--- gary_maker/df_maker.py
+++ gary_maker/df_maker.py
@@ -1,4 +1,9 @@
 import pandas as pd
 import numpy as np
 def df_maker(per_file, fam_file, per_vars, fam_vars, per_pos, fam_pos):
-    
+
+    fam_dict = dict(zip(fam_vars, fam_pos))
+    df = pd.read_fwf(fam_file, colspecs=fam_pos, header=None, names=fam_vars)
+    return df
+
+
