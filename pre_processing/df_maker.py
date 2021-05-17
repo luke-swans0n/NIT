be8952a7d5a91729a72cd19a8eb47f8ea06ec62b
The_Administrator <mr.luke.swanson@gmail.com>
2021-05-17 07:44:48 -0500

    Adding documentation, restructuring

--- pre_processing/df_maker.py
+++ pre_processing/df_maker.py
@@ -3,10 +3,16 @@ import numpy as np
 import configparser
 import os
 import glob
+
+from numpy import diff
+
+
 def df_maker(per_file, fam_file, per_vars, fam_vars, per_pos, fam_pos):
     os.chdir('../')
-    df = pd.read_fwf(fam_file, colspecs=fam_pos, header=None, names=fam_vars)
-    return df
+    fam_df = pd.read_fwf(fam_file, colspecs=fam_pos, header=None, names=fam_vars)
+    per_df = pd.read_fwf(per_file, colspecs= per_pos, header=None, names=per_vars)
+    print(fam_df.columns, per_df.columns)
+    return per_df.set_index("FAMNUM").join(fam_df.set_index("FAMNUM"), how="left")
 def main():
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -15,7 +21,8 @@ def main():
     fam_var = fam_var.split(",")
     per_pos, fam_pos = [x for x in tuple(map(eval, per_pos.split('\n')))], [x for x in
                                                                         tuple(map(eval, fam_pos.split("\n")))]
-    df = df_maker(per_file, fam_file, per_var, fam_var, per_pos, fam_pos)
+    df = df_maker(per_file, fam_file, per_var.split(',\n'), fam_var, per_pos, fam_pos)
     print(df)
+    return df
 if __name__ == '__main__':
     main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NIT',
+    version='1.00',
+    packages=[''],
+    url='https://github.com/luke-swans0n/NIT',
+    license='GNU',
+    author='Luke Swanson',
+    author_email='mr.luke.swanson@gmail.com',
+    description='An Examination of the Negative Income Tax Experiments'
+)
